3518f5b6d48b94cd11176e384db7202485ca14e8
a-heart3 <17302241931@163.com>
2023-08-02 09:48:18 +0800

    AList version1

--- AList.c
+++ AList.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdbool.h>
 #define MAX 100
 
 typedef struct AList {
@@ -25,7 +26,7 @@ int AddLast (AList* L, int e) {
 	L->size = L->size + 1;
 }
 
-int GetElm (AList L, int e) {
+int LocateElem (AList L, int e) {
 	for(int i = 0; i < L.size; i++) {
 		if(L.list[i] == e) {
 			printf("the %dth position is %d\n", i, e);
@@ -35,8 +36,61 @@ int GetElm (AList L, int e) {
 	return 0;
 }
 
+bool IsPosition(int size, int pos) {
+	if (pos > size || pos < 1) {
+		printf("error: the position is error");
+		return false;
+	}
+	return true;
+}
 
+int GetElem (AList L, int pos) {
 
+	if (!IsPosition(L.size, pos)) {
+		return -1;
+	}
+
+	return L.list[pos - 1];
+}
+
+int Insert (AList* L, int pos, int e) {
+	if(!IsPosition(L->size + 1, pos)) {
+		return -1;
+	}
+
+	if(L->size == MAX){
+		printf("list is full\n");
+		return -1;
+	}
+
+	for(int i = L->size; i >= pos; i--) {
+		L->list[i] = L->list[i - 1];
+	}
+
+	L->list[pos - 1] = e;
+	L->size = L->size + 1;
+
+	return 1;
+}
+
+int Delete (AList *L, int pos) {
+	if(L->size == 0) {
+		printf("list is empty\n");
+		return -1;
+	}
+
+	if(!IsPosition(L->size, pos)) {
+		return -1;
+	}
+
+	int element = L->list[pos - 1];
+	for(int i = pos - 1; i < L->size -1; i++) {
+		L->list[i] = L->list[i + 1];
+	}
+	L->size = L->size - 1;
+
+	return element;
+}
 
 int PrintList (AList L){
 	printf("your list name is: ");
@@ -63,12 +117,14 @@ int main(void) {
 	char name[50] = "ax";
 	Init(&l, name);
 	AddLast(&l, 100);
+	AddLast(&l, 200);
+	AddLast(&l, 300);
+	Insert(&l, 4, 1000);
+	Insert(&l, 2, 500);
 	PrintList(l);
-
-	printf("%d\n", l.size);
-	printf("%c %c\n", l.name[0], l.name[1]);
-
-
+	Delete(&l, 2);
+        printf("%d\n", GetElem(l, 2));
+	PrintList(l);
 
 	return 0;
 }
